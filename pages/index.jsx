5efada78a3cc506d3bef66583560948afc3202d3
André Menezes <andre.menezes@vnda.com.br>
2022-01-29 03:21:31 -0300

    Permitir escolher um cavaleiro no login

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,161 +2,117 @@ import { Box, Button, Text, TextField, Image } from '@skynexui/components'
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import appConfig from '../config.json';
+import Select from '../src/components/Select';
 import Title from '../src/components/Title';
 
-export default function HomePage() {
+export default function HomePage({ cavesData }) {
   const router = useRouter();
-  const [ userName, setUserName ] = useState('');
-  const userURL = `https://api.github.com/users/${userName}`
-  const [ userBio, setUserBio ] = useState('');
-  
-  function handleChange(event) {
-    setUserName(event.target.value)
+  const [ character, setCharacter ] = useState('');
+  const [ backgroundImage, setBackgroundImage ] = useState('/images/caves.gif');
 
-    if (event.target.value.length > 2) {
-      fetch(userURL)
-        .then(response => response.json())
-        .then(data => setUserBio(data.bio))
+  function handleChange(event) {
+    const { value } = event.target
+    setCharacter(value)
+
+    const currentChatacter = cavesData.find(
+      character => character.name.toLowerCase() === value.toLowerCase(
+    ))
+
+    if (currentChatacter) {
+      const currentChatacterImage = currentChatacter.image
+      
+      const imageURL = `https://diegochagas.com/saint-seiya-api/${currentChatacterImage}`
+  
+      setBackgroundImage(imageURL)
     }
   }
 
-  const imageError = 'https://images.unsplash.com/photo-1579373903781-fd5c0c30c4cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&h=400&q=80';
-
   return (
-    <>
+    <Box
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[100],
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundBlendMode: 'multiply',
+      }}
+    >
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[100],
-          backgroundImage: `url(/images/caves.gif)`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundBlendMode: 'multiply',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: {
+            xs: 'column',
+            sm: 'row',
+          },
+          minWidth: '400px',
+          borderRadius: '5px',
+          padding: {
+            xs: '32px',
+            sm: '64px'
+          },
+          margin: '16px',
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
         }}
       >
+        {/* Formulário */}
         <Box
+          as="form"
+          onSubmit={event => {
+            event.preventDefault()
+            router.push(`/chat?user=${character}`)
+          }}
           styleSheet={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            flexDirection: {
-              xs: 'column',
-              sm: 'row',
-            },
-            width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
-            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+            textAlign: 'center'
           }}
         >
-          {/* Formulário */}
-          <Box
-            as="form"
-            onSubmit={event => {
-              event.preventDefault()
-              router.push(`/chat?user=${userName}`)
-            }}
+          <Title tag="h2">{appConfig.title}</Title>
+          <Text
+            variant="body3"
             styleSheet={{
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              margin: '16px 0',
+              color: appConfig.theme.colors.neutrals[300]
             }}
           >
-            <Title tag="h2">Boas vindas de volta!</Title>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-              {appConfig.name}
-            </Text>
+            {appConfig.subtitle}
+          </Text>
 
-            <TextField
-              fullWidth
-              value={userName}
-              onChange={handleChange}
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
-              }}
-            />
+          <Select
+            cavesData={cavesData}
+            character={character}
+            handleChange={handleChange}
+          />
 
-            <Button
-              type='submit'
-              label='Entrar'
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
-          </Box>
-          {/* Formulário */}
-
-
-          {/* Photo Area */}
-          <Box
-            styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: '1px solid',
-              borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
-              flex: 1,
-              minHeight: '240px',
+          <Button
+            type='submit'
+            label='Entrar'
+            fullWidth
+            buttonColors={{
+              contrastColor: appConfig.theme.colors.neutrals["000"],
+              mainColor: appConfig.theme.colors.primary[500],
+              mainColorLight: appConfig.theme.colors.primary[400],
+              mainColorStrong: appConfig.theme.colors.primary[600],
             }}
-          >
-            <Image
-              styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
-              }}
-              src={
-                userName.length > 2
-                  ? `https://github.com/${userName}.png`
-                  : imageError
-              }
-            />
-
-            {
-              userName.length > 2 && (
-                <>
-                  <Text
-                    variant="body4"
-                    styleSheet={{
-                      color: appConfig.theme.colors.neutrals[200],
-                      backgroundColor: appConfig.theme.colors.neutrals[900],
-                      padding: '3px 10px',
-                      borderRadius: '1000px'
-                    }}
-                  >
-                    {userName}
-                  </Text>
-
-                  <Text
-                    variant="body4"
-                    styleSheet={{
-                      color: appConfig.theme.colors.neutrals[200],
-                      backgroundColor: appConfig.theme.colors.neutrals[900],
-                      padding: '3px 10px',
-                      borderRadius: '1000px'
-                    }}
-                  >
-                    {userBio}
-                  </Text>
-                </>
-              )
-            }
-          </Box>
-          {/* Photo Area */}
+          />
         </Box>
       </Box>
-    </>
+    </Box>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://saint-seiya-api.herokuapp.com/api/characters')
+  const rawData = await response.json()
+  const cavesData = await rawData.filter(character => character.name)
+
+  return {
+    props: { cavesData }
+  }
 }
---
+++ src/components/Select.jsx
@@ -0,0 +1,31 @@
+import { TextField } from '@skynexui/components';
+import React from 'react'
+import appConfig from '../../config.json';
+
+export default function Select({ cavesData, character, handleChange }) {
+  const dataListList = cavesData.map(character => (
+    <option key={character.id} value={character.name} />
+  ))
+
+  console.log('cavesData[389]:', cavesData[389])
+
+  return (
+    <>
+      <datalist id="caves-characteres">{dataListList}</datalist>
+      <TextField
+        list='caves-characteres'
+        fullWidth
+        value={character}
+        onChange={handleChange}
+        textFieldColors={{
+          neutral: {
+            textColor: appConfig.theme.colors.neutrals[200],
+            mainColor: appConfig.theme.colors.neutrals[900],
+            mainColorHighlight: appConfig.theme.colors.primary[500],
+            backgroundColor: appConfig.theme.colors.neutrals[800],
+          }
+        }}
+      />
+    </>
+  )
+}
